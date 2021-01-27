bdc3b2b0762e2a8749cab4851ca5dd106585ad82
cjrhansen2 <cjrhansen2@gmail.com>
2021-01-26 18:11:43 -0800

    trying to make jawsdb work

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "burgers_db"
+        host: "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "tckq4ut6rrj1yt8w",
+        password: "fo70uoy6mbw48ux2",
+        database: "ixo5himha8n9q50a"
     });
 }
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,5 @@
 /*Create the database and use it*/
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
+USE ixo5himha8n9q50a;
 /*Then make a table with the required*/
 CREATE TABLE burgers
 (
